e68a07daf8f9e0221cdfde19450118b3fc0a3eb6
twber <hctsai@umich.edu>
2020-08-29 23:32:11 +0800

    Initial Commit

---
+++ lifePathNum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifePathNum
+//
+//  Created by Bernice TSAI on 2020/8/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
